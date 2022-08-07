66b6790feb18e89b03148ad6b69f5f497a617738
Freddie <freddie.kenyon98@gmail.com>
2022-08-07 14:57:19 +0100

    Add styling, rainbow mode and separate functions in js file

--- script.js
+++ script.js
@@ -3,30 +3,34 @@ let squaresNumber = 100;
 let rainbowMode = 0;
 
 //functions
-function makeGrid() {
-    for (let i = 0;i < Math.sqrt(squaresNumber); i++) {
-        const row = document.createElement('div');
-        row.className = 'row';
-        row.setAttribute(
+function makeRow() {
+    const row = document.createElement('div');
+    row.className = 'row';
+    row.setAttribute(
             'style',
             'display: flex; width:960px;height 30px; justify-content: center')
-        for (let i = 0;i <  Math.sqrt(squaresNumber); i++) {
-            const div = document.createElement('div');
+    return row;
+}
+
+function makeGridBoxes(row) {
+    const div = document.createElement('div');
             div.className = 'gridBox';
             div.setAttribute(
                 'style',
                 'width: 30px; height: 30px; background: aqua; flex: 0 1 auto;')
             row.appendChild(div);
-        }
+}
+
+function makeGrid() {
+    for (let i = 0;i < Math.sqrt(squaresNumber); i++) {
+        const row = makeRow();
+        for (let i = 0;i <  Math.sqrt(squaresNumber); i++) {
+            makeGridBoxes(row);
         const container = document.querySelector('.squaresContainer');
         container.append(row);
-        const gridBoxes = document.querySelectorAll('.gridBox'); //draw functionality
-        Array.from(gridBoxes).forEach(gridBox =>
-            gridBox.addEventListener('click', function(){
-            gridBox.style.backgroundColor = 'red';
-            })
-        )
-    }}
+        }
+    }
+}
 
 function deleteGrid() {
     const rows = document.querySelectorAll('.row');
@@ -34,12 +38,33 @@ function deleteGrid() {
     Array.from(rows).forEach(row =>
         container.removeChild(row))
     }
-        
-makeGrid(); //draw initial grid
 
-const resetButton = document.querySelector('button'); //reset button
+function resetDraw() {
+    const gridBoxes = document.querySelectorAll('.gridBox'); //draw functionality
+    Array.from(gridBoxes).forEach(gridBox =>
+        gridBox.replaceWith(gridBox.cloneNode(true))
+    )
+}
+function setDraw() {
+    const gridBoxes = document.querySelectorAll('.gridBox'); //draw functionality
+    Array.from(gridBoxes).forEach(gridBox =>
+        gridBox.addEventListener('mouseover', function(){
+            if (rainbowMode === 0) {
+                gridBox.style.backgroundColor = 'rgb(255, 0, 0)';
+            }
+            else if (rainbowMode === 1) {
+                let rgbA = Math.floor((Math.random()*255)+1);
+                let rgbB = Math.floor((Math.random()*255)+1);
+                let rgbC = Math.floor((Math.random()*255)+1);
+                gridBox.style.backgroundColor = `rgb(${rgbA}, ${rgbB}, ${rgbC})`;
+            }
+        })
+    )
+}
+
+const resetButton = document.querySelector('.reset'); //reset button
 resetButton.addEventListener('click', function() {
-    let userNumber = parseInt(prompt('Choose a number'));
+    let userNumber = parseInt(prompt('Choose your preferred number of squares:'));
     for (let i = 0; i < 1;) {
         if (typeof(userNumber) == 'number') {
             if (userNumber > 100) {
@@ -53,4 +78,20 @@ resetButton.addEventListener('click', function() {
         }
     deleteGrid();
     makeGrid();
+    setDraw();
     }})
+
+const rainbowButton = document.querySelector('.toggle');
+rainbowButton.addEventListener('click', function() {
+    if (rainbowMode !== 1) {
+        rainbowMode = 1;
+    } else {
+        rainbowMode = 0;
+    };
+    resetDraw();
+    setDraw();
+})
+
+makeGrid(); //draw initial grid
+resetDraw();
+setDraw();
